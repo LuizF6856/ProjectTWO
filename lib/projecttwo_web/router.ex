3e051e7a518d57283696e557a9296e1c8748549c
Luiz Felipe da Silva Pereira <felipe.pereira@SUMUP.LOCAL>
2021-06-02 13:43:13 -0300

    route to  powerlist created

--- lib/projecttwo_web/router.ex
+++ lib/projecttwo_web/router.ex
@@ -8,22 +8,8 @@ defmodule ProjecttwoWeb.Router do
   scope "/api", ProjecttwoWeb do
     pipe_through :api
 
-    resources "/users", UserController, except: [:new, :edit]    
-  end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-      live_dashboard "/dashboard", metrics: ProjecttwoWeb.Telemetry
-    end
+    resources "/users", UserController, except: [:new, :edit]  
+    
+    get "powerlist/:numbers", PotenciaController, :list
   end
 end
