6cb8be1a0a6e63cc72800e721e0388c59a5cb74e
LuizF6856 <luizfellipe6843@gmail.com>
2021-07-13 19:02:05 -0300

    Automate test for function powerlist

--- test/projecttwo/powerlist_test.exs
+++ test/projecttwo/powerlist_test.exs
@@ -1,11 +1,13 @@
 defmodule Projecttwo.PowerListTest do
     use ExUnit.Case
 
+    @string_numbers "1234"
+
     test "happy power list" do
-        assert Projecttwo.PowerList.power_of_list("1234") == [1,4,9,16] 
+        assert Projecttwo.PowerList.power_of_list(@string_numbers) == [1,4,9,16] 
     end
 
     test "bad path power list" do
-        assert Projecttwo.PowerList.power_of_list("1234") != [2,4,6,8]
+        assert Projecttwo.PowerList.power_of_list(@string_numbers) != [1,8,27,64]
     end
 end
